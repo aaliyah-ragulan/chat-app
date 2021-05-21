2bb383e630a8c16901909d215421fcb75dbefdf9
ragulan <ragulanravi21@gmail.com>
2021-05-21 13:44:27 -0400

    added message comp functionality.

--- src/Channel.js
+++ src/Channel.js
@@ -1,5 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useState, useRef } from 'react'
+import PropTypes from 'prop-types'
 import firebase from 'firebase/app'
+import { useFirestoreQuery } from '../hooks'
+// Components
+import Message from './Message'
+
+// passing message comp inside talking channel
 
 const Channel = () => {
   return <div></div>
